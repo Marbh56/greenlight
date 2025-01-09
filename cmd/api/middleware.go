e969eb757ad0a2fdd5433115208b7151c58b1e9c
Marbh56 <nathan.a.sanders2@gmail.com>
2025-01-08 21:34:31 -0800

    split auth middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -126,7 +126,7 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 	})
 }
 
-func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+func (app *application) requireAuthenticatedUser(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user := app.contextGetUser(r)
 
@@ -134,7 +134,15 @@ func (app *application) requireActivatedUser(next http.HandlerFunc) http.Handler
 			app.authenticationRequiredResponse(w, r)
 			return
 		}
-		
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
 		if !user.Activated {
 			app.inactiveAccountResponse(w, r)
 			return
@@ -142,4 +150,6 @@ func (app *application) requireActivatedUser(next http.HandlerFunc) http.Handler
 
 		next.ServeHTTP(w, r)
 	})
+
+	return app.requireAuthenticatedUser(fn)
 }
